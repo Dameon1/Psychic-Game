1fba8d93331d33e6753cbfc2e6515f4e7d668b23
Dameon1 <dameonmedoza@gmail.com>
2017-11-08 09:40:34 -0700

    -1!
    >starter<functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,73 @@
 
-    var guessesLeft = [""]
-    var computerChoices = ["a", "b", "c", "d","e"];
-    var userGuess = [""];
-    var wins = 0;
-    var losses = 0;
-
-//working key event for wins
-  document.onkeyup = function(event) {
-    userGuess = event.key;
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-      if (userGuess === computerGuess){
-          console.log("yeas"), wins++; }
 
 
-var html = wins;
+        var guessesLeft = 9;
+        var computerChoices = ["a", "b", "c", "d","e"];
+        var userGuess = [""];
+        var wins = 0;
+        var losses = 0;
 
-document.querySelector("#wins").innerHTML = html;
+    //working key event for wins
+      document.onkeyup = function(event) {
+        userGuess = event.key;
+        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+          if (userGuess === computerGuess) {
+              console.log("yeas"), wins++;
+              var htmlwins = wins;
+              document.querySelector("#wins").innerHTML = htmlwins;
+            }
+             else (guessesLeft--);
+      var htmlguessesLeft = guessesLeft;
+      document.querySelector("#guessesLeft").innerHTML = htmlguessesLeft;
+    }
+
+      /*determines {guesses left counter: a number counter that goes down if wrong}
+      { show player guesses: An array that uses the .push feature
+        and display it on screen}
+      {wins++ losses--}*/
+
+// Start Game
+// Player picks a letter to start game
+//Computer picks a letter
+// letters are then compared , action taken, results
+//game over
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//nonworking key event for  displayed wins
+
+
+
+    /*
+      let htmllettersPicked = guessed;
+      document.querySelector("#guessesLeft").innerHTML = guessed;
+      document.querySelector("#guessesLeft").innerHTML = htmlguessesLeft;
+      document.querySelector("#wins").innerHTML = htmlwins;
+      document.querySelector("#losses").innerHTML = htmllosses;
+
 
   /*determines {guesses left counter: a number counter that goes down if wrong}
   { show player guesses: An array that uses the .push feature
