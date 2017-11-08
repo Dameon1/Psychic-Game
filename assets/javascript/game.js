c416b2ad57e93d9232a338e63136632d65f6426e
Dameon1 <dameonmedoza@gmail.com>
2017-11-08 15:17:59 -0700

    117
    idk

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,56 @@
-
-
-
+/* declared some variables*/
         var guessesLeft = 9;
         var computerChoices = ["a", "b", "c", "d","e"];
-        var userGuess = [""];
+        var userGuess = ["userguess"];
         var wins = 0;
         var losses = 0;
 
-    //working key event for wins
-
-
-
-
-
-
-
+/* made an action happen to start the game*/
 
       document.onkeyup = function(event) {
 
+/* set variables for userGuess and computerGuess*/
+
+      var userGuess = event.key;
+      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+      console.log(userGuess);
+
+/* Setting the action in motion by asking if player wins or losses or pickes again??*/
+
+      if ( userGuess === computerGuess) {
+          console.log("yeas"),
+          wins++;
+
+/*variables for resetting htmlValues(unset yet)*/
+
+          var htmlwins = wins;
+            document.querySelector("#wins").innerHTML = htmlwins;
+          var htmlguessesLeft = guessesLeft;
+            document.querySelector("#guessesLeft").innerHTML = htmlguessesLeft;
+/* needs querySelector for resetting letters picked*/
+}
 
 
 
-        var userGuess = event.key;
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-          if ( userGuess === computerGuess) {
-              console.log("yeas"), wins++;
-              var htmlwins = wins;
-              document.querySelector("#wins").innerHTML = htmlwins;
-              var htmlguessesLeft = guessesLeft;
-              document.querySelector("#guessesLeft").innerHTML = htmlguessesLeft;
-              var htmlguessesLeft = 9;
-              document.querySelector("#guessesLeft").innerHTML = htmlguessesLeft;
-
-              var htmlLosses = losses;
-              document.querySelector("#losses").innerHTML = htmlLosses;
-              }
-
-
-              else if (guessesLeft === 0){
-                console.log("no");losses++;
-              }
-
-                      else if (guessesLeft === 0) {
-                        guessesLeft--;
-                      var htmlguessesLeft = 9;
-                      document.querySelector("#guessesLeft").innerHTML = htmlguessesLeft;
-
-              }
-
-
-
-
-
-          else { guessesLeft--;
+/* this takes away by reducing guessesLeft*/
+      else  {
+           guessesLeft--;
+            console.log(guessesLeft);
+            console.log(htmlguessesLeft);
             var htmlguessesLeft = guessesLeft;
-          document.querySelector("#guessesLeft").innerHTML = htmlguessesLeft;
-}
-}
+            document.querySelector("#guessesLeft").innerHTML = htmlguessesLeft;
 
+/*If didnt win  */
+
+              if ( guessesLeft === 0 ) {
+                console.log("lose");
+                losses++;
+                var htmlLosses = losses;
+                  document.querySelector("#losses").innerHTML = htmlLosses;
+                var htmlguessesLeft = 9;
+                document.querySelector("#guessesLeft").innerHTML = htmlguessesLeft;
+                /*add querySelectorfor letters picked being reset*/
+}}}
 
       /*determines {guesses left counter: a number counter that goes down if wrong}
       { show player guesses: An array that uses the .push feature
